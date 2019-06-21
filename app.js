1443907b4be0c3e9e1a4c72c0f9dced6cb7dd48f
agungTuanany <agung.tuanany@gmail.com>
2019-06-21 16:51:31 +0700

    Using express-handlebars template engine

--- app.js
+++ app.js
@@ -1,34 +1,31 @@
 const express = require('express');
+// express-handlebars
+const exphbs = require('express-handlebars');
+
 const app = express();
 
-const port = 5000;
-
-/*
-  Middleware usually use for authentication password.
-*/
-// How middleware works
-app.use(function(req, res, next) {
-  //console.log(Date.now());
-  req.name = 'Agung Tuanany';
-  next();
-  // example of loading a series of middleware
-},function(req, res, next) {
-  console.log('Request URL: will return', req.originalUrl);
-  next();
-});
+// Handlebars Middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 // Index Route
 app.get('/', (req, res) => {
-  console.log(req.name);
-  //res.send('INDEX');
-  res.send(req.name);
+  // Handlebars change 'send' response to 'render'
+  res.render('index');
 });
 
 // About Route
 app.get('/about', (req, res) => {
-  res.send('ABOUT');
+  const title = 'Rendering from the server';
+  const title1 = 'is this rendered';
+  // Handlebars change 'send' response to 'render'
+  res.render('about', {
+    title,
+    title1
+  });
 })
 
+const port = 5000;
 app.listen(port, () => {
   console.log(`server started on ${port}`);
 })
