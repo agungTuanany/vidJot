74406b2c89fdc12c4d6e9fec669bdc7a956525b0
agungTuanany <agung.tuanany@gmail.com>
2019-06-21 15:08:06 +0700

    basic routing

--- app.js
+++ app.js
@@ -3,6 +3,20 @@ const app = express();
 
 const port = 5000;
 
+/*
+basic route in express have a method 'get' and 'post'. in every method have a callback function (handler functions)
+*/
+
+// Index Route
+app.get('/', (req, res) => {
+  res.send('INDEX');
+});
+
+// About Route
+app.get('/about', (req, res) => {
+  res.send('ABOUT');
+})
+
 app.listen(port, () => {
   console.log(`server started on ${port}`);
 })
